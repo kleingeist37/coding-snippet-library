b3cd94cd20276fd12453f445b5e667050e7ecdb0
kleingeist37 <104618584+kleingeist37@users.noreply.github.com>
2022-06-22 21:25:32 +0200

    Update syncValidator.angular.ts

--- angular/examples/syncValidator.angular.ts
+++ angular/examples/syncValidator.angular.ts
@@ -1,12 +1,13 @@
 export class CustomValidators {
 
-  private static forbiddenNamesList : string[] = [
-    'uglyName'
-  ]
-  public static isForbiddenName(control?: AbstractControl) : ValidationErrors |null {
+ public static isForbiddenName(control?: AbstractControl) : ValidationErrors |null {
+    const forbiddenNamesList: string[] = [
+      'uglyName'
+    ];
+    
     if(!control ||!control.value ||!control.dirty)
       return null;
     
-    return {isForbiddenName : { valid : this.forbiddenNamesList.some(x => x.toLowerCase() == control.value)}}
+    return forbiddenNamesList.some(x => x.toLowerCase() == control.value.toLowerCase()) ? {isForbiddenName : { valid : false}} : null;
   }
 }
